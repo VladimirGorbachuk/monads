c1d40f1302a3e13d01925aec6c12fb9760fbe3cd
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-26 22:26:29 +0300

    working

--- src/monads/monads.py
+++ src/monads/monads.py
@@ -36,6 +36,7 @@ class LazyEvalMonadWithException:
         ) -> None:
         self._value = value
         self._exception = exception
+        print("current bind stack", bind_stack)
         if bind_stack is None:
             self._bind_stack = []
         else:
