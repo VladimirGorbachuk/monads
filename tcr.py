458496b24196f66f1f924578648ec29ad4d52903
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-26 14:33:07 +0300

    working

--- tcr.py
+++ tcr.py
@@ -50,14 +50,16 @@ class AsyncMonadWithException:
     async def get_value(self) -> Any:
         if self._exception:
             raise self._exception
-        return await self._coroutine
+        if self._coroutine:
+            return await self._coroutine
+        return self._value
     
     def async_bind(self, func: Coroutine) -> "AsyncMonadWithException":
-        if not self._coroutine:
-            self._coroutine = func(self._value)
-            return self
         async def new_coroutine():
-            res = await self._coroutine
-            return await func(res)
-        self._coroutine = new_coroutine
-        return self
+            value = await self.get_value()
+            try:
+                new_value = await func(value)
+                return AsyncMonadWithException(value=new_value, coroutine=self._coroutine, exception=self._exception)
+            except Exception as e:
+                return AsyncMonadWithException(value=self._value, coroutine=self._coroutine, exception = e)
+        return AsyncMonadWithException(value=self._value, exception=self._exception, coroutine=new_coroutine)
