33ff7b200ffad0dec261de8db52357f32584af0a
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-27 01:16:58 +0300

    working

--- src/monads/monads.py
+++ src/monads/monads.py
@@ -92,7 +92,11 @@ class AsyncMonadWithException:
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, AsyncMonadWithException):
-            return self._value == other._value and self._coroutine == other._coroutine and self._exception == other._exception
+            return (
+                self._value == other._value 
+                and self._coroutine == other._coroutine 
+                and self._exception == other._exception
+            )
         return False
 
     async def get_value(self) -> Any:
