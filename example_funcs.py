bae12471fdd87fdf2e167e5f72e8a4c80019dbf5
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-26 13:10:03 +0300

    working

--- example_funcs.py
+++ example_funcs.py
@@ -2,6 +2,6 @@ def factorial(number: int) -> int:
     if number < 0:
         raise ValueError("cannot get factorial of a negative integer")
     result = 1
-    for i in range(1, n+1):
+    for i in range(1, number+1):
         result *= i
     return result
