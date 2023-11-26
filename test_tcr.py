cec2d1549855fe58dc4ea3a6ba7b2596be492265
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-26 14:56:46 +0300

    working

--- test_tcr.py
+++ test_tcr.py
@@ -5,35 +5,6 @@ import pytest
 import pytest_asyncio
 
 
-def test_wtf():
-    assert 1==1
-
-
-def test_wtf_2():
-    assert wtf(2, 3) == 5
-
-
-def test_wtf_3():
-    assert wtf("a", "b") == "ab"
-
-
-def test():
-    assert "" == ""
-
-
-def test_omg():
-    omg = Omg()
-    assert omg + omg == omg
-    assert omg + 1 == omg
-    assert omg - 1 == omg
-
-
-def test_ooo():
-    assert 1==1
-    assert 2==2
-    assert 3==3
-
-
 def test_monad():
     monad = MonadWithException(value=1)
     res = monad.bind(lambda x: x)
