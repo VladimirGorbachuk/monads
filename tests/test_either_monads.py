1fa1c5018cb09168d277be46fcd2390e62bc7022
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-27 22:28:03 +0300

    working

--- tests/test_either_monads.py
+++ tests/test_either_monads.py
@@ -25,4 +25,19 @@ def test_left_binds_left_func():
     assert new_instance.value == 2
 
 
+def _either_binding_callable_zero_division(monad: EitherMonad) -> EitherMonad:
+    if monad.is_right:
+        try:
+            return EitherMonad(value=1/monad.value, either_enum=EitherMonadEnum.RIGHT)
+        except ZeroDivisionError as e:
+            return EitherMonad(value=e, either_enum=EitherMonadEnum.LEFT)
+    else:
+        return monad
+
+
+def test_zero_division_either_monadic_callable_for_right() -> EitherMonad:
+    monad = EitherMonad(value=1, either_enum=EitherMonadEnum.RIGHT)
+    assert monad.bind(_either_binding_callable_zero_division).is_right is True
+
+
 # the tests above are not true either (funcs should return either monad, not just process values)
