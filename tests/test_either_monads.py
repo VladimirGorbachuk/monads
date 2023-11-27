d5690220e347907b5b48e4b3a95ec6e9a3294e48
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-11-27 21:04:41 +0300

    still strange approach to either - need to reconsider

--- tests/test_either_monads.py
+++ tests/test_either_monads.py
@@ -9,3 +9,17 @@ def test_instantiates_is_right():
 def test_instantiates_not_is_right():
     either_instance = EitherMonad(value=3, either_enum=EitherMonadEnum.LEFT)
     assert either_instance.is_right is False
+
+
+def test_binds_right():
+    either_instance = EitherMonad(value=3, either_enum=EitherMonadEnum.RIGHT)
+    new_instance = either_instance.bind_either(lambda x: x-1, lambda x: x+1)
+    assert new_instance.is_right is True
+    assert new_instance.value == 4
+
+
+def test_binds_left():
+    either_instance = EitherMonad(value=3, either_enum=EitherMonadEnum.LEFT)
+    new_instance = either_instance.bind_either(lambda x: x-1, lambda x: x+1)
+    assert new_instance.is_right is False
+    assert new_instance.value == 2
