3acfea3c819febd655d4cae24155b4fb970fd9b1
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2023-12-16 21:33:40 +0300

    working

--- src/monads/type_monad.py
+++ src/monads/type_monad.py
@@ -2,7 +2,11 @@ from typing import Generic, TypeVar
 
 
 ValueType = TypeVar("ValueType")
+CallableBoundType = TypeVar("CallableBoundType")
 
 
 class MonadType(Generic[ValueType]):
     _value: ValueType
+
+    def bind(self, func: CallableBoundType) -> "MonadType":
+        raise NotImplementedError
